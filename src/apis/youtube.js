6e341e51245736ed357c521f84d71646b78f2bb1
Joma Ejercito <jomaejercito@gmail.com>
2019-09-26 00:37:26 -0400

    Set up axios and youtube api

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyCqDjuliEu5M6cuC8wWCP-aPSHFchTqIk8';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
